ecb4b78e305bb139a6be94f5e5bc5394596af559
wparedes <wparedesgt@gmail.com>
2020-08-13 07:59:02 -0600

    Intro

---
+++ 01_Semana/01_Intro.R
@@ -0,0 +1,31 @@
+#Intro
+
+#¡Bienvenido al aprendizaje automático práctico! Este curso se enfoca en desarrollar las herramientas y técnicas para comprender, construir y probar funciones de predicción.
+
+#Estas herramientas están en el centro de la revolución de la ciencia de datos. A muchos investigadores, empresas y organizaciones gubernamentales les gustaría utilizar los datos baratos y abundantes que están recopilando para predecir qué les gustará a los clientes, qué servicios ofrecerán o cómo mejorar la vida de las personas.
+
+#El énfasis de este curso es el aprendizaje y la implementación prácticos. Si te gusta lo que aprendes en esta clase, hay una gran cantidad de otros MOOC de aprendizaje automático y estadístico que puedes utilizar para profundizar tu conocimiento de los detalles técnicos y matemáticos.
+
+#Consulte el programa del curso para obtener información sobre los cuestionarios, el proyecto y la calificación. No olvide saludar en los foros de mensajes. ¡La comunidad desarrollada en torno a estos cursos es uno de los mejores lugares para aprender y lo mejor de tomar un MOOC!
+  
+#Los cuestionarios 2-4 requieren que ejecute paquetes específicos. Los detalles del paquete se indican en los cuestionarios. Asegúrese de estar ejecutando el paquete correcto para obtener las respuestas que estamos buscando.
+
+#Jeff Leek y el equipo de seguimiento de ciencia de datos
+
+
+#Una de las tareas más comunes que realizan los científicos y analistas de datos es la predicción y el aprendizaje automático. Este curso cubrirá los componentes básicos de la construcción y aplicación de funciones de predicción con énfasis en aplicaciones prácticas. El curso proporcionará una base básica en conceptos tales como entrenamiento y conjuntos de pruebas, sobreajuste y tasas de error. El curso también presentará una variedad de métodos de aprendizaje automático algorítmicos y basados en modelos, que incluyen regresión, árboles de clasificación, Naive Bayes y bosques aleatorios. El curso cubrirá el proceso completo de creación de funciones de predicción, incluida la recopilación de datos, la creación de funciones, los algoritmos y la evaluación.
+
+
+#Course Content
+
+#Prediction study design
+#In sample and out of sample errors
+#Overfitting
+#Receiver Operating Characteristic (ROC) curves
+#The caret package in R
+#Preprocessing and feature creation
+#Prediction with regression
+#Prediction with decision trees
+#Prediction with random forests
+#Boosting
+#Prediction blending
